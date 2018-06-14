ca0f7c51bfceeae333636aae767b533cb0aedbea
Sandy Novelli <sandy_novelli@hotmail.com>
2018-06-13 23:04:20 -0300

    Commit Caixa Eletronico

---
+++ ATM/CaixaEletronico.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATM
+{
+    public class CaixaEletronico
+    {
+        public string Sacar(int valorDesejado)
+        {
+            int[] notasDisponiveis = { 100, 50, 20, 10 };
+            string saque = string.Empty;
+            int i = 0;
+
+            if (valorDesejado <= 0)
+                return saque = "Valor indisponível";
+            else
+            {
+                while (i < notasDisponiveis.Length)
+                {
+                    if (valorDesejado >= notasDisponiveis[i])
+                    {
+                        if (saque == string.Empty)
+                            saque = "Entregar " + (valorDesejado / notasDisponiveis[i]).ToString() + " nota(s) de R$" + notasDisponiveis[i].ToString() + ",00";
+                        else if ((valorDesejado % notasDisponiveis[i]) >= 10)
+                        {
+                            saque += ", " + (valorDesejado / notasDisponiveis[i]).ToString() + " nota(s) de R$" + notasDisponiveis[i].ToString() + ",00";
+                        }
+                        else
+                            saque += " e " + (valorDesejado / notasDisponiveis[i]).ToString() + " nota(s) de R$" + notasDisponiveis[i].ToString() + ",00";
+
+                        valorDesejado = valorDesejado % notasDisponiveis[i];
+                    }
+                    i++;
+                }
+                if (valorDesejado != 0)
+                    saque = "Não há notas disponíveis para realizar este saque";
+                return saque;
+            }
+        }
+    }
+}
---
+++ CaixaEletronicoTestes/CaixaEletronicoTeste.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ATM;
+
+namespace CaixaEletronicoTestes
+{
+    /// <summary>
+    /// Summary description for CaixaEletronicoTeste
+    /// </summary>
+    [TestClass]
+    public class CaixaEletronicoTeste
+    {
+        string saque = string.Empty;
+        
+        [TestMethod]
+        public void SaqueValor0()
+        {
+            CaixaEletronico objCaixa = new CaixaEletronico();
+            saque = objCaixa.Sacar(0);
+            Assert.AreEqual("Valor indisponível", saque);
+        }
+        [TestMethod]
+        public void SaqueNota100_1()
+        {
+            CaixaEletronico objCaixa = new CaixaEletronico();
+            saque = objCaixa.Sacar(100);
+            Assert.AreEqual("Entregar 1 nota(s) de R$100,00", saque);
+        }
+        [TestMethod]
+        public void SaqueNota50_1()
+        {
+            CaixaEletronico objCaixa = new CaixaEletronico();
+            saque = objCaixa.Sacar(50);
+            Assert.AreEqual("Entregar 1 nota(s) de R$50,00", saque);
+        }
+        [TestMethod]
+        public void Saque95()
+        {
+            CaixaEletronico objCaixa = new CaixaEletronico();
+            saque = objCaixa.Sacar(95);
+            Assert.AreEqual("Não há notas disponíveis para realizar este saque", saque);
+        }
+        [TestMethod]
+        public void Saque30()
+        {
+            CaixaEletronico objCaixa = new CaixaEletronico();
+            saque = objCaixa.Sacar(30);
+            Assert.AreEqual("Entregar 1 nota(s) de R$20,00 e 1 nota(s) de R$10,00", saque);
+        }
+        [TestMethod]
+        public void Saque80()
+        {
+            CaixaEletronico objCaixa = new CaixaEletronico();
+            saque = objCaixa.Sacar(80);
+            Assert.AreEqual("Entregar 1 nota(s) de R$50,00, 1 nota(s) de R$20,00 e 1 nota(s) de R$10,00", saque);
+        }
+        [TestMethod]
+        public void Saque890()
+        {
+            CaixaEletronico objCaixa = new CaixaEletronico();
+            saque = objCaixa.Sacar(890);
+            Assert.AreEqual("Entregar 8 nota(s) de R$100,00, 1 nota(s) de R$50,00 e 2 nota(s) de R$20,00", saque);
+        }
+    }
+}
